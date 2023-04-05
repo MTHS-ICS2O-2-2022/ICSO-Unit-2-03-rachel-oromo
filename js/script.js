fb8c4722d6f1ac3e3a93ad45f1ea0696a2e54ee3
rachel-oromo <124389469+rachel-oromo@users.noreply.github.com>
2023-04-05 16:46:22 +0000

    update changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Rachel Oromo All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Rachel Oromo
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+/**
+ * This function gets users first name and age and shows it back to user.
+ */
+function enterClicked() {
+  // input
+  const firstName = document.getElementById("first-name").value
+  const userAge = parseInt(document.getElementById("age-entered").value)
+
+  // output
+  document.getElementById("address").innerHTML =
+    "Your info is: " + firstName + ", age " + userAge + "."
+}
